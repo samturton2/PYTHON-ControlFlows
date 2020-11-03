bb77fb9a97da5cdf5d8d5ef4578ff2bd204fe3b4
samturton2 <STurton@spartaglobal.com>
2020-11-03 18:06:57 +0000

    find birthyear of user added

---
+++ find_birthdate_of_user.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+
+# Define age and name of user
+age = int(input("please enter your age: "))
+name = input("whats your name: ")
+
+# Ask if their birthday has happened this year
+ask = input("Have you had your birthday this year yet? ")
+# use their answer to account for birthyear
+
+if ask[0].lower() == 'y':
+    birthyear = int(datetime.today().year) - age
+else:
+    birthyear = int(datetime.today().year) - (age + 1)
+
+# Print out their name and predicted birth year
+print(f"OMG {name} you are {age} years old and you were born in {birthyear}.")
+
